ecd2285eb88f23206568512f4cd3750b6c1f72ca
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-01-21 17:00:45 +0000

    run closer dimer hessian

--- hessians/dimer_hessian.sh
+++ hessians/dimer_hessian.sh
@@ -5,9 +5,9 @@
 #SBATCH --time=0-01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --mem=5G
+#SBATCH --mem=25G
 
 export MKL_THREADING_LAYER=TBB
 export OMP_NUM_THREADS=1
 
-~/.local/src/Qcore/release/bin/qcore dimer_hessian.in > dimer_hessian.out
+~/.local/src/Qcore/release/bin/qcore -s "hess := hessian(structure(file='${DIMER}') xtb(model='chlorophyll') save_normal_modes = '${DIMER/xyz/molden}')" >  ${DIMER/xyz/out}
