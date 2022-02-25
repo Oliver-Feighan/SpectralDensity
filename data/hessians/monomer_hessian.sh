3956e82c62b8ad2e233fed5c1c1d33a4375abc3c
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-02-25 16:19:32 +0000

    rerun geom opt and hessian for truncated chl

--- data/hessians/monomer_hessian.sh
+++ data/hessians/monomer_hessian.sh
@@ -10,6 +10,6 @@
 export MKL_THREADING_LAYER=TBB
 export OMP_NUM_THREADS=1
 
-MONOMER=opt_bchla.xyz
+MONOMER=opt_truncated.xyz
 
 ~/.local/src/Qcore/release/bin/qcore -s "hess := hessian(structure(file='${MONOMER}') xtb(model='chlorophyll') save_normal_modes = '${MONOMER/xyz/molden}')" >  ${MONOMER/xyz/out}
--- data/hessians/opt_geom.sh
+++ data/hessians/opt_geom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=opt_geom
-#SBATCH --output=opt_geom_run.out
+#SBATCH --output=opt_trunc_geom_run.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=0-01:00:00
@@ -10,4 +10,4 @@
 export MKL_THREADING_LAYER=TBB
 export OMP_NUM_THREADS=20
 
-~/.local/src/Qcore/release/bin/qcore opt_geom.in > opt_geom.out
+~/.local/src/Qcore/release/bin/qcore opt_trunc_geom.in > opt_trunc_geom.out
