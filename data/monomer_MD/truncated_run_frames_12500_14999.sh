57ff279467c6a4acaf10c6cae7349505496f03c1
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-03-08 19:58:47 +0000

    truncated chl trans. prop.

---
+++ data/monomer_MD/truncated_run_frames_12500_14999.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=run_frames_12500_14999
+#SBATCH --output=run_frames_12500_14999.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=5
+#SBATCH --time=0-03:00:00
+#SBATCH --mem=15gb
+
+
+export OMP_NUM_THREADS=1
+export MKL_THREADING_LAYER=TBB
+
+hostname
+
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "Job started"
+echo "$dt"
+
+module load lang/python/anaconda/3.8-2020.07
+
+source activate openmm
+
+export QCORE_PATH=~/.local/src/Qcore/release/bin/qcore
+export FRAME_START=12500
+export FRAME_END=14999
+export TRAJ_FILE=truncated_trajectory.xyz 
+export N_ATOMS=79
+
+time python ../../src/run_xyz_traj.py
