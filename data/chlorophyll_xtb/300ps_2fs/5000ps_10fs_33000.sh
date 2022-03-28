5215f8091a922cc1291e2c7f7117cfe642251cfe
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-03-28 10:48:41 +0100

    300ps LHII output files

---
+++ data/chlorophyll_xtb/300ps_2fs/5000ps_10fs_33000.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#SBATCH --job-name=300ps_2fs
+#SBATCH --output=300ps_2fs_33000.out
+#SBATCH --time=0-20:00:00
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=50G
+
+module load lang/python/anaconda/3.8-2020.07
+
+source activate openmm
+
+which python
+
+export QCORE_PATH=~/.local/src/Qcore/release/bin/qcore
+export DCD_FILE=../../../LHII_MD/output/chl_300ps_2fs_LHII.dcd
+export PRMTOP_FILE=../../../LHII_MD/LH2.prmtop
+export ONLY_CHL=True
+
+export FRAME_START=33000
+export FRAME_END=36000
+
+export OMP_NUM_THREADS=1
+export MKL_THREADING_LAYER=TBB
+
+time python ../../../src/chl_xtb.py 
+
