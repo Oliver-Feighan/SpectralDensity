2bb2d74f706d81651ebfe9c982fa7ebd79d334bb
Oliver-Feighan <oliverfeighan1996@gmail.com>
2022-06-23 13:13:38 +0100

    300ps 2fs ether data

---
+++ data/ether_chl_xtb/300ps_2fs/ether_140000.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#SBATCH --job-name=ether_140000
+#SBATCH --output=ether_140000.out
+#SBATCH --time=0-03:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=35gb
+
+module load lang/python/anaconda/3.8-2020.07
+
+source activate openmm
+
+export QCORE_PATH=~/.local/src/Qcore/release/bin/qcore
+
+export DCD_FILE=../../../Ether_MD/output/300ps_2fs_ether.dcd
+export PRMTOP_FILE=../../../Ether_MD/forcefield_prep/bchla_in_diethyl_ether.prmtop
+
+export FRAME_START=140000
+export FRAME_END=145000
+
+export OMP_NUM_THREADS=1
+export MKL_THREADING_LAYER=TBB
+
+time python ../../../src/run_ether_traj.py
